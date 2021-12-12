f3a9a3cc11552f383fce676b2cbcc83a6ca2aef8
NickPustovoy <pustovoy.kolya@mail.ru>
2021-12-12 16:31:53 +0300

    Summary

---
+++ DZ3_IVT-21B_Pustovoy N.A._Var10/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DZ3_IVT_21B_Pustovoy_N.A._Var10
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private string getAnswer(string input)
+        {
+            string output = "";
+            string[] words = input.Split(" ");
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = 0; j < words[i].Length; j++)
+                {
+                    output += words[i][words[i].Length - j - 1];
+                }
+                output += " ";
+            }
+
+            return output;
+        }
+
+        private void bt_Click(object sender, RoutedEventArgs e)
+        {
+            string input, output;
+
+            input = tbtxt.Text;
+            output = getAnswer(input);
+
+            tbtxt.Text = "";
+            tbres.Text = output;
+        }
+    }
+}
